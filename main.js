00cd6950b1b735349ca298d92b1a98664fe15913
SaanviPatnaik8711 <115956391+SaanviPatnaik8711@users.noreply.github.com>
2023-07-17 16:01:20 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function preload(){
+
+}
+function setup(){
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    canvas.position(613, 200)
+}
+
+function draw(){
+
+}
+
+function take_snapshot(){
+    save("myFilterImage.png");
+}
